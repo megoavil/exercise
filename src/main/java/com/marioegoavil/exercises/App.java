9f87ffad8f1c4af2e7f3e854b47c40ce86792b5e
mario <mario@DESKTOP-QAEV7FD.fritz.box>
2023-10-08 11:35:30 -0500

    Added the sumOfThreeNumbers exercise

--- src/main/java/com/marioegoavil/exercises/App.java
+++ src/main/java/com/marioegoavil/exercises/App.java
@@ -1,5 +1,7 @@
 package com.marioegoavil.exercises;
 
+import java.util.Arrays;
+
 /**
  * Solutions for exercises
  *
@@ -19,6 +21,26 @@ public class App
     	return true;
     }
     
+    public static boolean sumOfThreeNumbers(int[] nums, int target) {
+    	if(nums.length<3) return false;
+    	Arrays.sort(nums);
+    	int i=0; //pointer to the base number
+    	int j, k; //pointers to the left and right pointers
+    	int temptarget;
+    	while(i<=nums.length-3) {
+    		//loop for shifting the target sum of values
+    		temptarget=target-nums[i];
+    		j=i+1;
+    		k=nums.length-1;
+    		while(j<k) {
+    			if(nums[j]+nums[k]==temptarget) return true;
+    			if(nums[j]+nums[k]<temptarget) j++;
+				if(nums[j]+nums[k]>temptarget)	k--;
+    		}
+    		i++;
+    	}
+    	return false;
+    }
 	
 	public static void main( String[] args )
     {
--- src/test/java/com/marioegoavil/exercises/AppTest.java
+++ src/test/java/com/marioegoavil/exercises/AppTest.java
@@ -22,4 +22,21 @@ public class AppTest
         assertEquals(true, App.isPalindrome("A") );
         assertEquals(false, App.isPalindrome("ABCDABCD") );
     }
+    
+    @Test
+    public void sumOfThreeNumbersTest()
+    {
+    	int[] intArray = new int[]{ 1,2,3 }; 
+    	assertEquals(true, App.sumOfThreeNumbers(intArray,6)) ;
+    	intArray = new int[]{ 1,-1,0 }; 
+    	assertEquals(false, App.sumOfThreeNumbers(intArray,-1)) ;
+    	intArray = new int[]{ 3,7,1,2,8,4,5 }; 
+    	assertEquals(true, App.sumOfThreeNumbers(intArray,10)) ;
+    	intArray = new int[]{ 3, 7, 1, 2, 8, 4, 5 }; 
+    	assertEquals(false, App.sumOfThreeNumbers(intArray,21)) ;
+    	intArray = new int[]{ -1, 2, 1, -4, 5, -3 }; 
+    	assertEquals(true, App.sumOfThreeNumbers(intArray,-8)) ;
+    	intArray = new int[]{ -1, 2, 1, -4, 5, -33 }; 
+    	assertEquals(true, App.sumOfThreeNumbers(intArray,0)) ;
+    }
 }
